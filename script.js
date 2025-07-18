4ec4454794ba0d7e7be5e18426c9b80181427ecc
VincECodesNicely <vk6953@hotmail.com>
2025-07-18 19:05:40 +1200

    random trail colour

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const container = document.querySelector(".container");
 const button = document.querySelector("#numSquares");
 
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256); // Red: 0–255
+  const g = Math.floor(Math.random() * 256); // Green: 0–255
+  const b = Math.floor(Math.random() * 256); // Blue: 0–255
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
 function createGrid(size) {
   container.innerHTML = "";
   const cellSize = 100 / size;
 
   for (let i = 0; i < size * size; i++) {
     let squares = document.createElement("div");
-    squares.addEventListener("mouseover", (e) =>
-      e.target.classList.add("my-colour-class")
+    squares.addEventListener(
+      "mouseover",
+      (e) => (e.target.style.background = getRandomColor())
     );
     squares.classList.add("div");
     squares.style.width = `${cellSize}%`;
