1cfd1a30a15ad0a9f7868269cad7ed21b725b7b7
VincECodesNicely <vk6953@hotmail.com>
2025-07-18 18:22:59 +1200

    compact grid, number squares functions, colour trail functions

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 const container = document.querySelector(".container");
 const button = document.querySelector("#numSquares");
 
-for (let i = 0; i < 257; i++) {
-  let div = document.createElement("div");
-  div.addEventListener("mouseover", (e) =>
-    e.target.classList.add("my-colour-class")
-  );
-  container.appendChild(div);
+function createGrid(size) {
+  container.innerHTML = "";
+  const cellSize = 100 / size;
+
+  for (let i = 0; i < size * size; i++) {
+    let squares = document.createElement("div");
+    squares.addEventListener("mouseover", (e) =>
+      e.target.classList.add("my-colour-class")
+    );
+    squares.classList.add("div");
+    squares.style.width = `${cellSize}%`;
+    squares.style.height = `${cellSize}%`;
+
+    container.appendChild(squares);
+  }
 }
+
+button.addEventListener("click", () => {
+  let input = parseInt(prompt("How many sqaure would you like to draw on?"));
+  if (input > 100) {
+    alert("cannot do more than 100 squares");
+    return;
+  }
+  createGrid(input);
+});
+
+window.onload = () => {
+  createGrid(16);
+};
